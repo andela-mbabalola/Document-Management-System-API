9351af3ad8b3d6eb8e27509fb44f32e7106d4662
Maryam <maryamolabisi02@gmail.com>
2015-12-10 15:15:18 +0100

    feat(app): add middlewares/auth.js file

---
+++ app/middlewares/auth.js
@@ -0,0 +1,56 @@
+var config = require('./../../config/config'),
+  jwt = require('jsonwebtoken'),
+  adminConfig = require('./../../config/adminConfig'),
+  appConfig = require('./../../config/appConfig');
+
+/**
+ * [function description]
+ * @param  {[type]}   req  [description]
+ * @param  {[type]}   res  [description]
+ * @param  {Function} next [description]
+ * @return {[type]}        [description]
+ */
+exports.authMiddleware = function(req, res, next) {
+  //check header, url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+  //decoding token
+  if (token) {
+    //verify secret and check exp
+    jwt.verify(token, config.secret, function(err, decoded) {
+      if (err) {
+        return res.json({
+          success: false,
+          message: 'Failed to authenticate token'
+        });
+      } else {
+        //if everything is good
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } else {
+    //if no token is found
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided'
+    });
+  }
+};
+
+/**
+ * [function description]
+ * @param  {[type]}   req  [description]
+ * @param  {[type]}   res  [description]
+ * @param  {Function} next [description]
+ * @return {[type]}        [description]
+ */
+exports.verifyAdmin = function(req, res, next) {
+  if (req.params.userName !== adminConfig.adminName) {
+    res.json({
+      success: false,
+      message: "Access denied!"
+    });
+  } else {
+    next();
+  }
+};
