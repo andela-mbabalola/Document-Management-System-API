1b393b36fa2277eaba699c29c97b757c29ec6259
Maryam <maryamolabisi02@gmail.com>
2015-12-16 18:42:02 +0100

    feat(spec): modify userTest.spec.js

--- spec/userTest.spec.js
+++ spec/userTest.spec.js
@@ -1,28 +1,31 @@
-var mongoose = require('mongoose'),
-  jwt = require('jsonwebtoken'),
-  fs = require('fs'),
-  server = require('./../server.js'),
-  request = require('supertest')(server),
-  user = require('./../app/models/user.models'),
-  role = require('./../app/models/role.models'),
-  userSeeders = fs.readFileSync(__dirname + '/../seeders/user.seeders.json'),
-  roleSeeders = fs.readFileSync(__dirname + '/../seeders/role.seeders.json'),
+var jwt = require("jsonwebtoken"),
+  fs = require("fs"),
+  expect = require("expect.js"),
+  server = require("./../server.js"),
+  request = require("supertest")(server),
+  user = require("./../app/models/user.models"),
+  role = require("./../app/models/role.models"),
+  config = require("./../config/config"),
+  userSeeders = fs.readFileSync(__dirname + "/../seeders/user.seeders.json"),
+  roleSeeders = fs.readFileSync(__dirname + "/../seeders/role.seeders.json"),
 
   _userSeeders = JSON.parse(userSeeders),
   _roleSeeders = JSON.parse(roleSeeders);
 
 describe("Users", function() {
-  describe("/POST: Validate user login", function() {  //   beforeEach(function(done) {
+  describe("/POST: Validate user login", function() {
+    before(function(done) {
       var testRole = new role(_roleSeeders[0]);
       testRole.save();
       // Update existing role string with the new found role object
       _userSeeders[0].role = testRole._id;
       var testUser = new user(_userSeeders[0]);
       testUser.save();
+      console.log("Created");
       done();
     });
 
-    afterEach(function(done) {
+    after(function(done) {
       user.remove({}, function() {
         role.remove({}, function() {
           done();
@@ -31,59 +34,59 @@ describe("Users", function() {
     });
 
     it("should connect to root", function(done) {
-      request.get('/')
+      request.get("/")
         .expect(200);
+      done();
     });
 
-    it("should login new users", function(done) {
-      request.post('/api/users/login')
+    it("should verify that user logs in with correct password", function(done) {
+      request.post("/api/users/login")
         .send({
           userName: _userSeeders[0].userName,
-          password: _userSeeders[0].password
+          password: "me"
         }).end(function(err, res) {
-          expect(res.body).toEqual(jasmine.objectContaining({
-            success: true,
-            message: 'Successfully logged in'
-          }));
-          expect(res.body).not.toBeNull();
-          expect(res.statusCode).toBe(200);
-          expect(err).toBe(null);
+          console.log(res.body);
+          expect(res.body.success).to.eql(false);
+          expect(res.body.message).to.eql("Authentication failed. Wrong password");
           done();
         });
     });
 
     it("should verify that user logs in with correct userName", function(done) {
-      request.post('/api/users/login')
+      request.post("/api/users/login")
         .send({
-          password: 'myName' || undefined,
-          userName: _userSeeders[0].password
+          userName: "myName",
+          password: _userSeeders[0].password
         }).end(function(err, res) {
-          expect(res.body).toEqual(jasmine.objectContaining({
-            success: false,
-            message: 'Authentication failed. User not found'
-          }));
-          done();
-        });
-    });
-  
-    it("should verify that user logs in with correct password", function(done) {
-      request.post('/api/users/login')
-        .send({
-          userName: _userSeeders[0].userName,
-          password: 'myPassword' || undefined
-        }).end(function(err, res) {
-          expect(res.body).toEqual(jasmine.objectContaining({
-            success: false,
-            message: 'Authentication failed. Wrong password'
-          }));
+          console.log(res.body);
+
+          expect(res.body.success).to.eql(false);
+          expect(res.body.message).to.eql("Authentication failed. User not found");
+
           done();
         });
     });
 
+    it("should login new users", function(done) {
+      request.post("/api/users/login")
+        .send({
+          userName: _userSeeders[0].userName,
+          password: _userSeeders[0].password
+        }).end(function(err, res) {
+          console.log(res.body.success);
+
+          expect(res.body.success).to.eql(true);
+          expect(res.body.message).to.eql("Successfully logged in");
+          expect(res.body.message).to.not.be.empty();
+
+
+          done();
+        });
+    });
   });
 
   describe("/POST: Create new users", function() {
-    beforeEach(function(done) {
+    before(function(done) {
       var testRole = new role(_roleSeeders[1]);
       testRole.save();
       // Update existing role string with the new found role object
@@ -94,7 +97,7 @@ describe("Users", function() {
     });
 
 
-    afterEach(function(done) {
+    after(function(done) {
       user.remove({}, function() {
         role.remove({}, function() {
           done();
@@ -103,8 +106,8 @@ describe("Users", function() {
     });
 
     it("create a new user", function(done) {
-      request.post('/api/users')
-        .set('Accept', 'x-www-form-urlencoded')
+      request.post("/api/users")
+        .set("Accept", "x-www-form-urlencoded")
         .send({
           firstName: _userSeeders[1].name.firstName,
           lastName: _userSeeders[1].name.lastName,
@@ -113,18 +116,16 @@ describe("Users", function() {
           email: _userSeeders[1].email,
           role: _userSeeders[1].role
         }).end(function(err, res) {
-          expect(res.body).toEqual(jasmine.objectContaining({
-            success: true,
-            message: 'User Successfully created!'
-          }));
-          expect(err).toBeNull();
-          expect(res.statusCode).toBe(200);
+
+          expect(res.body.success).to.eql(true);
+          expect(res.body.message).to.eql("User Successfully created!");
+
           done();
         });
     });
 
     it("Does not create a user without a role", function(done) {
-      request.post('/api/users')
+      request.post("/api/users")
         .send({
           firstName: _userSeeders[1].name.firstName,
           lastName: _userSeeders[1].name.lastName,
@@ -133,36 +134,32 @@ describe("Users", function() {
           email: _userSeeders[1].email,
           role: "Owner" || undefined
         }).end(function(err, res) {
-          expect(res.body).toEqual(jasmine.objectContaining({
-            success: false,
-            message: 'Role not found. Create first'
-          }));
+
+          expect(res.body.success).to.eql(false);
+          expect(res.body.message).to.eql("Role not found. Create first");
+
           done();
         });
     });
 
-    it("Does not create a user without a firstName and lastName", function(done) {
-      request.post('/api/users')
+    it("checks for before creating firstName and lastName", function(done) {
+      request.post("/api/users")
         .send({
-          firstName: undefined,
-          lastName: undefined,
           userName: _userSeeders[1].userName,
           password: _userSeeders[1].password,
           email: _userSeeders[1].email,
           role: _userSeeders[1].role
         }).end(function(err, res) {
-          expect(err).not.toBeNull();
-          expect(res.statusCode).not.toBe(200);
+          console.log(res.body.message);
+
+          expect(res.body.message).to.eql("User validation failed");
+          expect(res.body.errors).to.not.be.empty();
+
+          done();
         });
-      done();
     });
 
   });
 
-  describe("/GET: Get all users", function () {
-    it("returns all the available users in the database", function (done) {
-
-    });
-  });
 
 });
