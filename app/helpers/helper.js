846c9b5e4fec809d063433f0a41e4b417e902e4e
Maryam <maryamolabisi02@gmail.com>
2016-01-05 10:14:54 +0100

    feat(app): modify helper.js

--- app/helpers/helper.js
+++ app/helpers/helper.js
@@ -1,5 +1,16 @@
-var bcrypt = require('bcrypt');
+(function() {
+  'Use strict';
 
-exports.comparePassword = function(password, hashedPassword) {
-  return bcrypt.compareSync(password, hashedPassword);
-};
+  var bcrypt = require('bcrypt');
+
+  /**
+   * [function to encrypt a password and compare with another]
+   * @param  {[String]} password       [password to be compared with]
+   * @param  {[String]} hashedPassword [encrypted password to be decrypted]
+   * @return {[boolean]}                [if passwords are the same or not]
+   */
+  exports.comparePassword = function(password, hashedPassword) {
+    return bcrypt.compareSync(password, hashedPassword);
+  };
+
+})();
