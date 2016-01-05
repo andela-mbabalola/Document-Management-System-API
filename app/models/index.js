7813a4afb435ad9ff511260418a1400048a2b2ea
Maryam <maryamolabisi02@gmail.com>
2016-01-05 10:16:22 +0100

    feat(app): modify index.js

--- app/models/index.js
+++ app/models/index.js
@@ -1,7 +1,12 @@
-var Role = require('./role.models'),
-  User = require('./user.models'),
-  Documents = require('./document.models');
+(function() {
+  'Use strict';
 
-exports.Role = Role;
-exports.User = User;
-exports.Documents = Documents;
+  var Role = require('./role.models'),
+    User = require('./user.models'),
+    Documents = require('./document.models');
+
+  exports.Role = Role;
+  exports.User = User;
+  exports.Documents = Documents;
+
+})();
