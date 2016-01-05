e191ce69481e6fc82cf316e4ed16820be3b4f1d5
Maryam <maryamolabisi02@gmail.com>
2016-01-05 10:14:00 +0100

    feat(app): modify role.controllers.js

--- app/controllers/role.controllers.js
+++ app/controllers/role.controllers.js
@@ -4,18 +4,20 @@
   var Role = require("./../models/role.models");
 
   /**
-   * [function description]
-   * @param  {[type]} req [description]
-   * @param  {[type]} res [description]
-   * @return {[type]}     [description]
+   * [function to create a role]
+   * @param  {[http request object]} req [used to get the request query]
+   * @param  {[http response object]} res [used to respond back to client ]
+   * @return {[json]}     [success message that role has been created]
    */
   exports.createRole = function(req, res) {
+    //check if role already exists
     Role.findOne({
       title: req.body.title
     }, function(err, role) {
       if (err) {
         res.send(err);
       } else {
+        //if role exists
         if (role) {
           res.json({
             success: false,
@@ -25,6 +27,7 @@
           var newRole = new Role({
             title: req.body.title
           });
+          //create a new role
           newRole.save(function(err) {
             if (err) {
               res.send(err);
@@ -40,10 +43,18 @@
     });
   };
 
+  /**
+   * [function to get all available roles]
+   * @param  {[http request object]} req [used to get the request query]
+   * @param  {[http response object]} res [used to respond back to client ]
+   * @return {[json]}     [all available roles in the database]
+   */
   exports.getAllRoles = function(req, res) {
+    //find all available roles
     Role.find({}, function(err, roles) {
       if (err) {
         res.send(err);
+        //if no role is found
       } else if (roles.length === 0) {
         res.json({
           success: false,
@@ -55,6 +66,12 @@
     });
   };
 
+  /**
+   * [function to get a specific role by it Id]
+   * @param  {[http request object]} req [used to get the request query]
+   * @param  {[http response object]} res [used to respond back to client ]
+   * @return {[json]}     [a role with a specific Id]
+   */
   exports.getRoleById = function(req, res) {
     Role.findById(req.params.id, function(err, role) {
       if (err) {
@@ -74,18 +91,20 @@
   };
 
   /**
-   * [function description]
-   * @param  {[type]} req [description]
-   * @param  {[type]} res [description]
-   * @return {[type]}     [description]
+   * [function to edit a role's detail]
+   * @param  {[http request object]} req [used to get the request query]
+   * @param  {[http response object]} res [used to respond back to client ]
+   * @return {[json]}     [success message that role has been updated]
    */
   exports.editRole = function(req, res) {
+    //find a role with its Id and update its content
     Role.findByIdAndUpdate(req.params.id, req.body, function(err, role) {
       if (err) {
         res.json({
           success: false,
           message: "Update role failed!"
         });
+        //if no role is found
       } else if (!role) {
         res.status(404).json({
           success: false,
@@ -101,18 +120,20 @@
   };
 
   /**
-   * [function description]
-   * @param  {[type]} req [description]
-   * @param  {[type]} res [description]
-   * @return {[type]}     [description]
+   * [function to delete a role]
+   * @param  {[http request object]} req [used to get the request query]
+   * @param  {[http response object]} res [used to respond back to client ]
+   * @return {[json]}     [success message that role has been deleted]
    */
   exports.deleteRole = function(req, res) {
+    //find a role with its Id and delete
     Role.findById(req.params.id).remove(function(err, role) {
       if (err) {
         res.json({
           success: false,
           message: "Delete role failed"
         });
+        //if no role is found
       } else if (!role) {
         res.status(404).json({
           success: false,
