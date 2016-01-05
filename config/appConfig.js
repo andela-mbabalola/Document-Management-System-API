4eb713f4267d756c8ac4780b65a7edc2df1ad574
Maryam <maryamolabisi02@gmail.com>
2016-01-05 10:10:15 +0100

    feat(config): modify appConfig.js

--- config/appConfig.js
+++ config/appConfig.js
@@ -1,27 +1,38 @@
-var express = require("express"),
-  app = express(),
-  router = express.Router(),
-  bodyParser = require("body-parser"),
-  morgan = require("morgan"),
-  methodOverride = require("method-override"),
-  routes = require("./../app/routes/index");
+(function() {
+  "Use strict";
 
-routes(router);
+  var express = require("express"),
+    //creating an instance of express
+    app = express(),
 
-app.use(methodOverride());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
-app.use(morgan("dev"));
+    bodyParser = require("body-parser"),
+    morgan = require("morgan"),
+    methodOverride = require("method-override"),
+    routes = require("./../app/routes/index"),
 
-app.get("/", function(req, res) {
-  res.send({
-    text: "You have successfully reached the test route",
-    code: 200
+  //mounting an instance of the express router on the routes
+  router = express.Router();
+    routes(router);
+
+  app.use(methodOverride());
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(morgan("dev"));
+
+  //creating a test route
+  app.get("/", function(req, res) {
+    res.send({
+      text: "You have successfully reached the test route",
+      code: 200
+    });
   });
-});
 
-app.use("/api", router);
+  //mounting the router on a /api directory
+  app.use("/api", router);
 
-module.exports = app;
+  //exporting app
+  module.exports = app;
+
+})();
