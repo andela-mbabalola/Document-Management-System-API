6b9eb9a358fc952a0e42471219fe1bc6490ea692
Maryam <maryamolabisi02@gmail.com>
2016-01-05 10:14:17 +0100

    feat(app): modify document.controllers.js

--- app/controllers/document.controllers.js
+++ app/controllers/document.controllers.js
@@ -13,38 +13,44 @@
    * @return {[json]}     [success message that document has been created]
    */
   exports.createDocument = function(req, res) {
+    //check if role exists
     Role.findOne({
       title: req.body.role
     }, function(err, role) {
       if (err) {
         res.send(err);
       } else {
+        //if role is not found
         if (!role) {
-          res.status(403).json({
+          res.status(404).json({
             success: false,
             message: "Role not found. Create first!"
           });
         } else {
+          //check if user exists
           User.findOne({
             userName: req.body.ownerId
           }, function(err, user) {
             if (err) {
               res.send(err);
             } else {
+              //if no user is found
               if (!user) {
-                res.json({
+                res.status(404).json({
                   success: false,
                   message: "User not found. Create first"
                 });
               } else {
+                //check if document already exists
                 Documents.findOne({
                   title: req.body.title
                 }, function(err, document) {
                   if (err) {
                     res.send(err);
                   } else {
+                    //if a document is found
                     if (document) {
-                      res.json({
+                      res.status(400).json({
                         success: false,
                         message: "Document already exists!"
                       });
@@ -55,6 +61,7 @@
                         ownerId: user.id,
                         role: role
                       });
+                      //create a new document
                       newDoc.save(function(err) {
                         if (err) {
                           res.send(err);
@@ -83,21 +90,21 @@
    * @return {[json]}     [all documents available in the database]
    */
   exports.getAllDocument = function(req, res) {
+    //find all available document(s)
     Documents.find({})
+    //parsing the limit
       .limit(parseInt(req.params.limit))
       .exec(function(err, docs) {
         if (err) {
           res.send(err);
+          //if no document is found
         } else if (!docs) {
-          res.json({
+          res.status(404).json({
             success: false,
             message: "No document available"
           });
         } else {
-          res.json({
-            success: true,
-            message: docs
-          });
+          res.status(200).json(docs);
         }
 
       });
@@ -110,9 +117,11 @@
    * @return {[json]}     [document with a specific Id]
    */
   exports.getADocument = function(req, res) {
+    //find a document with a specific id
     Documents.findById(req.params.id, function(err, doc) {
       if (err) {
         res.send(err);
+        //if no document is found
       } else if (!doc) {
         res.status(404).json({
           success: false,
@@ -132,15 +141,18 @@
    */
 
   exports.getDocumentByRole = function(req, res) {
+    //find documents with a specific role
     Documents.find({
         role: req.params.role
       })
+      //parsing the limit
       .limit(parseInt(req.params.limit))
       .exec(function(err, doc) {
         if (err) {
           res.send(err);
+          //if no document is found
         } else if (!doc) {
-          res.json({
+          res.status(404).json({
             success: false,
             message: "Role has no document"
           });
@@ -158,13 +170,16 @@
    */
 
   exports.getDocumentByUser = function(req, res) {
+    //find documents with a specific user
     Documents.find({
         ownerId: req.params.ownerId
       })
+      //parsing the limit
       .limit(parseInt(req.params.limit))
       .exec(function(err, doc) {
         if (err) {
           res.send(err);
+          //if no document is found
         } else if (doc.length < 1) {
           res.status(404).json({
             success: false,
@@ -183,13 +198,15 @@
    * @return {[json]}     [success message that document has been updated]
    */
   exports.editDocument = function(req, res) {
+    //find a document with a specific Id and update
     Documents.findByIdAndUpdate(
       req.params.id, req.body,
       function(err, document) {
         if (err) {
           res.send(err);
+          //if no document is found
         } else if (!document) {
-          res.json({
+          res.status(404).json({
             success: false,
             message: "Document does not exist"
           });
@@ -209,16 +226,18 @@
    * @return {[json]}     [success message that document has been deleted]
    */
   exports.deleteDocument = function(req, res) {
+    //find a document with a specific Id and delete it
     Documents.findByIdAndRemove(req.params.id, function(err, document) {
       if (err) {
         res.send(err);
+        //if no document is found
       } else if (!document) {
         res.status(404).json({
           success: false,
           message: "Document not found"
         });
       } else {
-        res.json({
+        res.status(200).json({
           success: true,
           message: "Document successfully deleted"
         });
