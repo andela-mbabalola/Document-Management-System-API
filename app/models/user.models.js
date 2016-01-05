51b3c219b73350b53fb1e6f8f73085c9da3b1db0
Maryam <maryamolabisi02@gmail.com>
2016-01-05 10:15:35 +0100

    feat(app): modify user.models.js

--- app/models/user.models.js
+++ app/models/user.models.js
@@ -1,124 +1,128 @@
-var mongoose = require("mongoose"),
-  bcrypt = require("bcrypt"),
-  SALT_WORK_FACTOR = 10,
-  Schema = mongoose.Schema,
-  ObjectId = Schema.Types.ObjectId;
+(function() {
+  "Use strict";
 
-var User = new Schema({
-  name: {
-    firstName: {
-      type: String,
-      required: true,
-      validate: {
-        validator: function(name) {
-          return /[a-zA-Z]/.test(name);
-        },
-        message: "{VALUE} is not a valid name!"
+  var mongoose = require("mongoose"),
+    bcrypt = require("bcrypt"),
+    SALT_WORK_FACTOR = 10,
+    Schema = mongoose.Schema,
+    ObjectId = Schema.Types.ObjectId;
+
+  var UserSchema = new Schema({
+    name: {
+      firstName: {
+        type: String,
+        required: true,
+        validate: {
+          validator: function(name) {
+            return /[a-zA-Z]/.test(name);
+          },
+          message: "{VALUE} is not a valid name!"
+        }
+      },
+
+      lastName: {
+        type: String,
+        required: true,
+        validate: {
+          validator: function(name) {
+            return /[a-zA-Z]/.test(name);
+          },
+          message: "{VALUE} is not a valid name!"
+        }
       }
     },
 
-    lastName: {
+    userName: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function(name) {
+          return /\w/.test(name);
+        },
+        message: "{VALUE} is not a valid userName!"
+      }
+    },
+
+    password: {
+      type: String,
+      required: true,
+      minLength: 8,
+      validate: {
+        validator: function(password) {
+          return /\w/.test(password);
+        },
+        message: "{VALUE} is not a valid password!"
+      }
+    },
+
+    email: {
       type: String,
       required: true,
       validate: {
-        validator: function(name) {
-          return /[a-zA-Z]/.test(name);
+        validator: function(email) {
+          return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(email);
         },
-        message: "{VALUE} is not a valid name!"
+        message: "{VALUE} is not a valid email!"
       }
+    },
+
+    role: {
+      type: ObjectId,
+      ref: "Role",
+      required: true
+    },
+
+    createdAt: {
+      type: Date,
+      default: Date.now
+    },
+
+    updatedAt: {
+      type: Date,
+      default: Date.now
     }
-  },
+  });
 
-  userName: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: {
-      validator: function(name) {
-        return /\w/.test(name);
-      },
-      message: "{VALUE} is not a valid userName!"
-    }
-  },
+  // Bcrypt middleware on UserSchema
+  UserSchema.pre("save", function(next) {
+    var user = this;
 
-  password: {
-    type: String,
-    required: true,
-    minLength: 8,
-    validate: {
-      validator: function(password) {
-        return /\w/.test(password);
-      },
-      message: "{VALUE} is not a valid password!"
-    }
-  },
-
-  email: {
-    type: String,
-    required: true,
-    validate: {
-      validator: function(email) {
-        return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(email);
-      },
-      message: "{VALUE} is not a valid email!"
-    }
-  },
-
-  role: {
-    type: ObjectId,
-    ref: "Role",
-    required: true
-  },
-
-  createdAt: {
-    type: Date,
-    default: Date.now
-  },
-
-  updatedAt: {
-    type: Date,
-    default: Date.now
-  }
-});
-
-// Bcrypt middleware on UserSchema
-User.pre("save", function(next) {
-  var user = this;
-
-  // only hash the password if it has been modified (or is new)
-  if (!user.isModified("password")) {
-    return next();
-  }
-
-  // generate a salt
-  bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-    if (err) {
-      return next(err);
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified("password")) {
+      return next();
     }
 
-    // hash the password using our new salt
-    bcrypt.hash(user.password, salt, function(err, hash) {
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
       if (err) {
         return next(err);
       }
 
-      // override the cleartext password with the hashed one
-      user.password = hash;
-      next();
+      // hash the password using our new salt
+      bcrypt.hash(user.password, salt, function(err, hash) {
+        if (err) {
+          return next(err);
+        }
+
+        // override the cleartext password with the hashed one
+        user.password = hash;
+        next();
+      });
     });
   });
-});
 
-User.methods.comparePassword = function(candidatePassword, cb) {
-  bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-    if (err) {
-      return cb(err);
-    }
-    cb(null, isMatch);
-  });
-};
+  UserSchema.methods.comparePassword = function(candidatePassword, cb) {
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+      if (err) {
+        return cb(err);
+      }
+      cb(null, isMatch);
+    });
+  };
 
 
-var User = mongoose.model("User", User);
-module.exports = User;
-// exports.User = User;
+  var User = mongoose.model("User", UserSchema);
+  module.exports = User;
+
+})();
