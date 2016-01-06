cfe0b35bb81b7a81ed80392f09eb31f99daf4126
Maryam <maryamolabisi02@gmail.com>
2016-01-06 11:50:08 +0100

    feat(app): modify user.routes.js

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -1,34 +1,31 @@
 (function() {
   'Use strict';
 
-var userController = require('./../controllers/user.controllers'),
-  auth = require('./../middlewares/auth');
+  var userController = require('./../controllers/user.controllers'),
+    auth = require('./../middlewares/auth');
 
-function userRoute(router) {
+  function userRoute(router) {
 
-  //route to login a user
-  router.route('/users/login')
-    .post(userController.login);
+    //route to login a user
+    router.route('/users/login')
+      .post(userController.login);
 
     //route to create a new user
-  router.route('/users')
-    .post(userController.createUser);
+    router.route('/users')
+      .post(userController.createUser);
 
-    //mounting the authMiddleware middleware
-  router.all('/*', auth.authMiddleware);
-
-  //route to get all available user(s)
-  router.route('/users')
-    .get(userController.getAllUsers);
+    //route to get all available user(s)
+    router.route('/users')
+      .get(auth.authMiddleware, userController.getAllUsers);
 
     //route to get, edit and delete a user specified by its Id
-  router.route('/users/:id')
-    .get(userController.getUserById)
-    .put(userController.updateUser)
-    .delete(userController.deleteUser);
+    router.route('/users/:id')
+      .get(auth.authMiddleware, userController.getUserById)
+      .put(auth.authMiddleware, userController.updateUser)
+      .delete(auth.authMiddleware, userController.deleteUser);
 
-}
-//exporting all available routes
-module.exports = userRoute;
+  }
+  //exporting all available routes
+  module.exports = userRoute;
 
 })();
