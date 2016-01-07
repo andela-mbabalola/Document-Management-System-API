5da442f95c4a8c33f0b52c5bf0e9a1bdefdce5e4
Maryam <maryamolabisi02@gmail.com>
2016-01-07 10:21:01 +0100

    feat(app): modify index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,15 +1,12 @@
 (function() {
   'Use strict';
-
   var _userRoute = require('./user.routes'),
     _documentRoute = require('./document.routes'),
     _roleRoute = require('./role.routes');
-
   var routes = function(router) {
     _userRoute(router);
     _documentRoute(router);
     _roleRoute(router);
   };
   module.exports = routes;
-
 })();
